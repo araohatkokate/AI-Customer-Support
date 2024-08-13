1a4aab76b6c22381c8f97c91328776871e5889c4
araohatkokate <araohatk@gmail.com>
2024-08-13 00:01:54 -0500

    created initial display message and button to send user prompt

--- app/page.js
+++ app/page.js
@@ -1,48 +1,53 @@
 'use client'
 import Image from "next/image";
-import {useState} from 'react'
+import { useState } from 'react'
+import { Box, Stack, TextField, Button } from '@mui/material'; // Assuming you're using Material UI
 
 export default function Home() {
   const [messages, setMessages] = useState([{
     role: 'assistant',
     content: `Hi I'm the HeadStarter Support Agent, how can I assist you today?`
-}])
+  }]);
 
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
 
-  return <Box 
-  width="100vw" 
-  height="100vh" 
-  display="flex" 
-  flexDirection="column"
-  justifyContent="center"
-  alignItems="center"
-  >
-    <Stack
-    direction="column"
-    width="600px"
-    height="700px"
-    border="1px solid black"
-    p={2}
-    spacing={3}>
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       <Stack
-      direction="column"
-      spacing={2}
-      flexGrow={1}
-      overflow="auto"
-      maxHeight="100%"
+        direction="column"
+        width="600px"
+        height="700px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
       >
-        {messages.map((message, index)=>(
+        <Stack
+          direction="column"
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {messages.map((message, index) => (
             <Box
-            key = {index} display='flex' justifyContent = {
-              message.role=== 'assistant' ? 'flex-start': 'flex-end'
-            }
+              key={index} 
+              display='flex' 
+              justifyContent={
+                message.role === 'assistant' ? 'flex-start' : 'flex-end'
+              }
             >
               <Box
                 bgcolor={
-                  message.role==='assistant'
-                  ? 'primary.main'
-                  : 'secondary.main'
+                  message.role === 'assistant'
+                    ? 'primary.main'
+                    : 'secondary.main'
                 }
                 color="white"
                 borderRadius={16}
@@ -51,9 +56,17 @@ export default function Home() {
                 {message.content}
               </Box>
             </Box>
-         ))
-        }
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField
+          label = "message"
+          fullWidth
+          value = {message}
+          onChange = {(e) => setMessage(e.target.value)}/>
+          <Button variant = "contained">Send</Button>
+        </Stack>
       </Stack>
-    </Stack>
-  </Box>
+    </Box>
+  );
 }
