263c48a8e37c9cbb218cc27582725c948979bd48
araohatkokate <araohatk@gmail.com>
2024-08-12 23:03:14 -0500

    working on main display of app

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 'use client'
 import Image from "next/image";
+import {useState} from 'react'
 
 export default function Home() {
+  const [messages, setMessages] = useState({
+    role: 'assistant',
+    content: `Hi I'm the HeadStarter Support Agent, how can I assist you today?`
+  })
+
+  const [message, setMessage] = useState('')
+
+  return 
 }
